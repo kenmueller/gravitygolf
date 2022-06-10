2d9f1caa08214a08a8229c3ec0bce01749d167d0
Ken Mueller <kenmueller0@gmail.com>
2022-06-10 10:29:50 -0700

    init canvas

--- routes/levels/[id].svelte
+++ routes/levels/[id].svelte
@@ -16,16 +16,52 @@
 <script lang="ts">
 	import type { Load } from '@sveltejs/kit'
 
+	import { browser } from '$app/env'
+
 	import levels from '$lib/level/levels.json'
 	import BackLink from '../../components/Link/Back.svelte'
 
 	export let id: number
+
+	let canvas: HTMLCanvasElement | null = null
+	$: context = canvas && canvas.getContext('2d')
+
+	let size: [number, number] | null = browser
+		? [window.innerWidth, window.innerHeight]
+		: null
+
+	$: scale = browser ? window.devicePixelRatio : null
+	$: scale && context?.scale(scale, scale)
+
+	const onResize = () => {
+		size = [window.innerWidth, window.innerHeight]
+	}
 </script>
+
+<svelte:window on:resize={onResize} />
 
 <svelte:head>
 	<title>Level {id}</title>
 </svelte:head>
 
-<BackLink href="/levels" />
+<main>
+	<canvas
+		bind:this={canvas}
+		width={size && scale && Math.floor(size[0] * scale)}
+		height={size && scale && Math.floor(size[1] * scale)}
+		style={size && `width: ${size[0]}px; height: ${size[1]}px;`}
+	/>
+	<BackLink href="/levels" />
+</main>
 
-<h1>Level {id}</h1>
+<style lang="scss">
+	main {
+		position: relative;
+
+		> :global(a) {
+			position: absolute;
+			top: 1rem;
+			left: 1rem;
+		}
+	}
+</style>
