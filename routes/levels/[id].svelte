ef26c440d730a6fc582d9baa1dd177aab369acc1
Ken Mueller <kenmueller0@gmail.com>
2022-06-10 10:13:58 -0700

    level page id guard

--- routes/levels/[id].svelte
+++ routes/levels/[id].svelte
@@ -1,5 +1,31 @@
-<script>
-	import BackLink from '../../components/Link/Back.svelte'
+<script context="module" lang="ts">
+	export const load: Load = ({ params }) => {
+		const id = Number.parseInt(params.id)
+
+		if (Number.isNaN(id)) return { status: 301, redirect: '/levels' }
+
+		if (id - 1 < 0) return { status: 301, redirect: '/levels' }
+		if (id - 1 >= levels.length) return { status: 307, redirect: '/levels' }
+
+		return {
+			props: { id }
+		}
+	}
 </script>
 
+<script lang="ts">
+	import type { Load } from '@sveltejs/kit'
+
+	import levels from '$lib/level/levels.json'
+	import BackLink from '../../components/Link/Back.svelte'
+
+	export let id: number
+</script>
+
+<svelte:head>
+	<title>Level {id}</title>
+</svelte:head>
+
 <BackLink href="/levels" />
+
+<h1>Level {id}</h1>
