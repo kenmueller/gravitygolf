ea00dd5929d70764182748752c446c521bcda7fe
Ken Mueller <kenmueller0@gmail.com>
2022-06-28 12:39:54 -0700

    sitemap

---
+++ routes/sitemap.xml.ts
@@ -0,0 +1,38 @@
+import type { RequestHandler } from '@sveltejs/kit'
+
+import levels from '$lib/level/levels'
+import errorFromValue from '../lib/error/from/value'
+
+const urls = [
+	'/',
+	'/levels',
+	...levels.map((_level, index) => `/levels/${index + 1}`)
+]
+
+const sitemap = (origin: string) =>
+	`<?xml version="1.0" encoding="UTF-8" ?>\
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" \
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \
+xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 \
+http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\
+${urls
+	.map(url => `<url><loc>${encodeURI(new URL(url, origin).href)}</loc></url>`)
+	.join('')}\
+</urlset>`
+
+let data: string | null = null
+
+export const get: RequestHandler = ({ url }) => {
+	try {
+		return {
+			headers: {
+				'cache-control': 'no-cache',
+				'content-type': 'application/xml'
+			},
+			body: (data ??= sitemap(url.origin))
+		}
+	} catch (value) {
+		const { code, message } = errorFromValue(value)
+		return { status: code, body: message }
+	}
+}
