6e7e9bc77c09d7a7ebfb90111b17b34c0680abac
Ken Mueller <kenmueller0@gmail.com>
2022-06-28 13:09:11 -0700

    maskable

--- routes/manifest.webmanifest.ts
+++ routes/manifest.webmanifest.ts
@@ -4,6 +4,7 @@ import type { WebAppManifest } from 'web-app-manifest'
 import errorFromValue from '$lib/error/from/value'
 
 import touch from '../images/touch.png'
+import mask from '../images/mask.png'
 
 const manifest: WebAppManifest = {
 	dir: 'ltr',
@@ -17,7 +18,10 @@ const manifest: WebAppManifest = {
 	theme_color: '#24292e',
 	background_color: '#24292e',
 	categories: ['games', 'entertainment', 'education'],
-	icons: [{ src: touch, sizes: '180x180' }]
+	icons: [
+		{ src: touch, sizes: '180x180' },
+		{ src: mask, sizes: '512x512', purpose: 'monochrome maskable' }
+	]
 }
 
 let data: string | null = null
