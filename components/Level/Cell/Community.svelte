fc757001514e541e29080700675a7c2f32770d9d
Ken Mueller <kenmueller0@gmail.com>
2022-07-05 08:54:21 -0700

    load stars

--- components/Level/Cell/Community.svelte
+++ components/Level/Cell/Community.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import type CommunityLevelRecord from '$lib/level/community/record'
+	import stars from '$lib/level/community/stars'
 	import Base from './Base.svelte'
 
 	export let level: CommunityLevelRecord
 </script>
 
-<Base href="/levels/community/{level.id}" stars={null}>
+<Base href="/levels/community/{level.id}" stars={$stars?.[level.id] ?? null}>
 	{level.name}
 </Base>
