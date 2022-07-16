03463ca2cce2ca016075e1e64120e546453682f7
Ken Mueller <kenmueller0@gmail.com>
2022-07-16 16:25:23 -0700

    import js

--- lib/hash.ts
+++ lib/hash.ts
@@ -1,5 +1,5 @@
-import AES from 'crypto-js/aes'
-import utf8 from 'crypto-js/enc-utf8'
+import AES from 'crypto-js/aes.js'
+import utf8 from 'crypto-js/enc-utf8.js'
 
 export const encrypt = (value: string) =>
 	AES.encrypt(value, import.meta.env.VITE_HASH).toString()
