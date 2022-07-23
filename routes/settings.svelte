4346efe7c10a7b43022b036c7f372b0804d473cb
Ken Mueller <kenmueller0@gmail.com>
2022-07-22 17:32:55 -0700

    sign out

--- routes/settings.svelte
+++ routes/settings.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
 	import mobile from '$lib/mobile'
+	import currentUser from '$lib/user/current'
+	import _signOut from '$lib/user/sign/out'
 	import deleteLevelProgress from '$lib/level/progress/remove'
+	import errorFromValue from '$lib/error/from/value'
 	import MetaImage from '../components/Meta/Image.svelte'
 	import MetaTitle from '../components/Meta/Title.svelte'
 	import MetaDescription from '../components/Meta/Description.svelte'
 	import BackLink from '../components/Link/Back.svelte'
+	import SignOut from '../images/SignOut.svelte'
 	import Trash from '../images/Trash.svelte'
+
+	let signOutLoading = false
+
+	const signOut = async () => {
+		try {
+			if (!$currentUser || signOutLoading) return
+			signOutLoading = true
+
+			await _signOut()
+		} catch (value) {
+			console.error(value)
+			alert(errorFromValue(value).message)
+		} finally {
+			signOutLoading = false
+		}
+	}
 
 	const deleteProgress = () => {
 		switch (prompt("Type 'delete' to delete all progress")) {
@@ -33,6 +53,16 @@
 		</BackLink>
 	</header>
 	<div>
+		{#if $currentUser}
+			<button
+				class="sign-out"
+				aria-busy={signOutLoading || undefined}
+				on:click={signOut}
+			>
+				<SignOut />
+				<span>Sign out</span>
+			</button>
+		{/if}
 		<button class="delete-progress" on:click={deleteProgress}>
 			<Trash />
 			<span>Delete all progress</span>
@@ -71,19 +101,20 @@
 		margin-top: 1.5rem;
 	}
 
+	.sign-out,
 	.delete-progress {
 		display: flex;
 		align-items: center;
 		max-width: 20rem;
 		width: 100%;
+		margin-top: 2rem;
 		padding: 0.5rem 0.5rem;
 		font-size: 1rem;
 		color: white;
-		background: rgb(222, 75, 70);
 		border-radius: 0.5rem;
 		transition: opacity 0.3s;
 
-		&:hover {
+		&:not([aria-busy]):hover {
 			opacity: 0.7;
 		}
 
@@ -94,5 +125,22 @@
 		> span {
 			margin: 0 auto;
 		}
+	}
+
+	.sign-out {
+		background: #ee9f0b;
+
+		> :global(svg) {
+			transform: translateY(0.05rem);
+		}
+	}
+
+	.delete-progress {
+		background: #de4b46;
+	}
+
+	[aria-busy] {
+		pointer-events: none;
+		opacity: 0.5;
 	}
 </style>
