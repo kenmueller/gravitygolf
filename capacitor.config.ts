5e1fef89703e495eb82ab2162e4d320de066e29a
Ken Mueller <kenmueller0@gmail.com>
2022-08-13 16:43:13 -0700

    capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+	appId: 'golf.grav.gravitygolf',
+	appName: 'Gravity Golf',
+	webDir: 'build',
+	bundledWebRuntime: false
+}
+
+export default config
